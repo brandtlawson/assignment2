73ec7701271eeea6313ac823fa8b9641ca5729a8
brandtlawson <43549359+brandtlawson@users.noreply.github.com>
2020-02-07 20:53:16 -0600

    Create test_assignment2.py

---
+++ tests/test_assignment2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""Tests for `assignment2` package."""
+
+
+import unittest
+from click.testing import CliRunner
+
+from assignment2 import assignment2
+from assignment2 import cli
+
+
+class TestAssignment2(unittest.TestCase):
+    """Tests for `assignment2` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+
+    def test_command_line_interface(self):
+        """Test the CLI."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main)
+        assert result.exit_code == 0
+        assert 'assignment2.cli.main' in result.output
+        help_result = runner.invoke(cli.main, ['--help'])
+        assert help_result.exit_code == 0
+        assert '--help  Show this message and exit.' in help_result.output
